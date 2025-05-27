dd4fdd13d35b2f4438a177cadc6cdf305d0b0ac9
Alan Daniel <20223tn018@utez.edu.mx>
2025-05-27 15:59:38 -0600

    [FIRST COMMIT] Subida de proyecto

---
+++ src/main/java/utez/edu/mx/cvadhr9b/Cvadhr9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvadhr9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvadhr9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvadhr9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvadhr9b/Cvadhr9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvadhr9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvadhr9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
