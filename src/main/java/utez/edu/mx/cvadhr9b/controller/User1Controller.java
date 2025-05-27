68197dc29344849d282643abadad8fb9734daf3a
Alan Daniel <20223tn018@utez.edu.mx>
2025-05-27 16:16:06 -0600

    add of endpoints

--- src/main/java/utez/edu/mx/cvadhr9b/controller/User1Controller.java
+++ src/main/java/utez/edu/mx/cvadhr9b/controller/User1Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/user1")
 public class User1Controller {
     @GetMapping("")
     public String getMessage() {
